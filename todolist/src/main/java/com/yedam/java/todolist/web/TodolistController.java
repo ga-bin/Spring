3f7a7597db7d0dfcfc5f5bbdd8d95a96e96475cc
gabin <rlarkqlskb@naver.com>
2022-09-14 16:29:32 +0900

    9/14 스프링 공부다시하자!

--- todolist/src/main/java/com/yedam/java/todolist/web/TodolistController.java
+++ todolist/src/main/java/com/yedam/java/todolist/web/TodolistController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yedam.java.todolist.service.TodoVO;
@@ -28,7 +29,7 @@ public class TodolistController {
 	
 	
 	@PostMapping("/todoList")
-	public int insertTodo(TodoVO todoVO){
+	public int insertTodo(@RequestBody TodoVO todoVO){
 		todoVO.setNo(service.insertNo());
 		return service.insertTodo(todoVO);
 	}
@@ -39,7 +40,7 @@ public class TodolistController {
 	}
 	
 	@PutMapping("/todoList")
-	public int updateTodo(TodoVO todoVO) {
+	public int updateTodo(@RequestBody TodoVO todoVO) {
 		return service.updateTodo(todoVO);
 	}
 }
